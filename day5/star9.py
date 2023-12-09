7e8c5a06fdc142fc5e84db91fba3355753412502
Lucky Horseshoe <anneta.volgina@mail.ru>
2023-12-09 07:31:59 +0300

    fix code

--- day5/star9.py
+++ day5/star9.py
@@ -1,38 +1,29 @@
 with open('input.txt', 'r') as f:
   lines = f.readlines()
-
+seeds, lines = lines[0], lines[2:]
 dic, new_dic = {}, {}
 key_strings = [
-    'seed-to-soil map:\n', 
-    'soil-to-fertilizer map:\n', 
-    'fertilizer-to-water map:\n', 
+    'seed-to-soil map:\n',
+    'soil-to-fertilizer map:\n',
+    'fertilizer-to-water map:\n',
     'water-to-light map:\n',
     'light-to-temperature map:\n',
     'temperature-to-humidity map:\n',
     'humidity-to-location map:\n'
 ]
-first, last = False, False
+last = False
+for x in seeds[seeds.find(':') + 1:].split():
+  dic[int(x)] = int(x)
 
 for line in lines:
-  if line.find('seeds') > -1:
-    first = True
-    for x in line[line.find(':') + 1:].split():
-        dic[int(x)] = -1
-    continue
   if line in key_strings:
-    continue
-  if first:
-    first = False
     continue
   if line == 'humidity-to-location map:\n':
     last = True
     continue
   if not len(line.split()) and not last:
     for key in dic.keys():
-      if dic[key] == -1:
-        new_dic[key] = -1
-      else:
-        new_dic[dic[key]] = -1
+      new_dic[dic[key]] = dic[key]
     dic = new_dic
     new_dic = {}
     continue
@@ -40,7 +31,4 @@ for line in lines:
   for key in dic.keys():
     if key >= src and key <= src + rng:
       dic[key] = dst + key - src
-for key in dic.keys():
-  if dic[key] == -1:
-    dic[key] = key
-print(min(dic.values()))
+print(min(dic.values())) #26273516
